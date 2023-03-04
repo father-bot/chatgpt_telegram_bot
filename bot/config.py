654963beba382f8b5d65cc22c521545da93d538e
Sergey Krashevich <svk@svk.su>
2023-03-04 12:29:18 +0300

    CI and docker

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 import dotenv
 from pathlib import Path
@@ -5,16 +6,18 @@ from pathlib import Path
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
 # load yaml config
-with open(config_dir / "config.yml", 'r') as f:
-    config_yaml = yaml.safe_load(f)
-
+try:
+    with open(config_dir / "config.yml", 'r') as f:
+        config_yaml = yaml.safe_load(f)
+except:
+    config_yaml = {}
 # load .env config
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
-use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
-allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
-new_dialog_timeout = config_yaml["new_dialog_timeout"]
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+telegram_token = os.getenv('TELEGRAM_TOKEN', config_yaml.get("telegram_token"))
+openai_api_key =  os.getenv('OPENAI_API_KEY', config_yaml.get("openai_api_key"))
+use_chatgpt_api =  os.getenv('USE_CHATGPT_API', config_yaml.get("use_chatgpt_api", True))
+allowed_telegram_usernames =  config_yaml.get('allowed_telegram_usernames', os.getenv("TELEGRAM_USERNAMES", "").split(','))
+new_dialog_timeout =  int(os.getenv("NEW_DIALOG_TIMEOUT", config_yaml.get("new_dialog_timeout")))
+mongodb_uri = "mongodb://mongo:"+config_env.get('MONGODB_PORT', "27017")

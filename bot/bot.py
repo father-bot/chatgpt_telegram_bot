2d68aa0611ffe0c73a2485204e2a0bbf44747ec3
Clement Sauvage <clement.sauvage@adevinta.com>
2023-03-10 15:23:07 +0100

    Add support for user ids

--- bot/bot.py
+++ bot/bot.py
@@ -308,10 +308,12 @@ def run_bot() -> None:
     )
 
     # add handlers
-    if len(config.allowed_telegram_usernames) == 0:
-        user_filter = filters.ALL
-    else:
+    user_filter = filters.ALL
+    if len(config.allowed_telegram_usernames) > 0 and isinstance(config.allowed_telegram_usernames[0], str):
         user_filter = filters.User(username=config.allowed_telegram_usernames)
+
+    if len(config.allowed_telegram_usernames) > 0 and isinstance(config.allowed_telegram_usernames[0], int):
+        user_filter = filters.User(user_id=config.allowed_telegram_usernames)
 
     application.add_handler(CommandHandler("start", start_handle, filters=user_filter))
     application.add_handler(CommandHandler("help", help_handle, filters=user_filter))

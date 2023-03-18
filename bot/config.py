b7464ff665f96fc2d8db18ace2fd3127767cf62f
Hongrui Chen <chraac@gmail.com>
2023-03-18 17:01:09 +0800

    remove redundant error handler

--- bot/config.py
+++ bot/config.py
@@ -20,7 +20,6 @@ new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
 sqlite_database_uri = config_env['SQLITE_DATABASE_PATH']
-ignore_network_error = config_yaml.get("ignore_network_error", False)
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:

95c664407b4d2e6733bd63a61d32e4c81e91e382
Santiago <s@yop.li>
2023-04-14 18:58:00 +0200

    Better config test

--- bot/config.py
+++ bot/config.py
@@ -1,29 +1,40 @@
+import os
 import yaml
-import dotenv
 from pathlib import Path
 
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
-# load yaml config
-with open(config_dir / "config.yml", 'r') as f:
-    config_yaml = yaml.safe_load(f)
+# load yaml config if it exists, else use environment variables
+config_yaml_file = config_dir / "config.yml"
+if config_yaml_file.exists():
+    with open(config_yaml_file, 'r') as f:
+        config_yaml = yaml.safe_load(f)
+else:
+    config_yaml = {}
 
-# load .env config
-config_env = dotenv.dotenv_values(config_dir / "config.env")
+# load .env config if it exists, else use environment variables
+config_env_file = config_dir / "config.env"
+if config_env_file.exists():
+    import dotenv
+    config_env = dotenv.dotenv_values(config_env_file)
+else:
+    config_env = {}
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
-use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
-allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
-new_dialog_timeout = config_yaml["new_dialog_timeout"]
-enable_message_streaming = config_yaml.get("enable_message_streaming", True)
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+telegram_token = config_yaml.get("telegram_token", os.environ.get("TELEGRAM_TOKEN"))
+openai_api_key = config_yaml.get("openai_api_key", os.environ.get("OPENAI_API_KEY"))
+use_chatgpt_api = config_yaml.get("use_chatgpt_api", os.environ.get("USE_CHATGPT_API", True))
+allowed_telegram_usernames = config_yaml.get("allowed_telegram_usernames", os.environ.get("ALLOWED_TELEGRAM_USERNAMES"))
+new_dialog_timeout = config_yaml.get("new_dialog_timeout", os.environ.get("NEW_DIALOG_TIMEOUT"))
+enable_message_streaming = config_yaml.get("enable_message_streaming", os.environ.get("ENABLE_MESSAGE_STREAMING", True))
+mongodb_uri = f"mongodb://mongo:{config_env.get('MONGODB_PORT', os.environ.get('MONGODB_PORT'))}"
 
 # chat_modes
-with open(config_dir / "chat_modes.yml", 'r') as f:
-    chat_modes = yaml.safe_load(f)
+chat_modes_file = config_dir / "chat_modes.yml"
+with open(chat_modes_file, 'r') as f:
+        chat_modes = yaml.safe_load(f)
 
 # models
-with open(config_dir / "models.yml", 'r') as f:
-    models = yaml.safe_load(f)
+models_file = config_dir / "models.yml"
+with open(models_file, 'r') as f:
+        models = yaml.safe_load(f)

aa546cdf95c7b3308978b3b041dbebf00682332f
Marco Noto <marcoemmanuelnoto@gmail.com>
2023-04-24 19:30:39 +0200

    *Connected to remote mongodb instance

--- bot/config.py
+++ bot/config.py
@@ -12,15 +12,15 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+telegram_token = config_env["TELEGRAM_TOKEN"]
+openai_api_key = config_env["OPENAI_API_KEY"]
 use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+mongodb_uri = config_env["MONGODB_URI"]
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:

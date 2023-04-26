2803f88d92a083b49d46decd1b54bdc1decbec09
Marco Noto <marcoemmanuelnoto@gmail.com>
2023-04-26 20:04:46 +0200

    * Changed env variable name to match that of railway

--- bot/config.py
+++ bot/config.py
@@ -19,12 +19,12 @@ openai_api_key = os.environ["OPENAI_API_KEY"]
 telegram_token = os.environ["TELEGRAM_TOKEN"]
 use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
 env_allowed_users = os.environ.get('ALLOWED_TELEGRAM_USERNAMES', '')
-allowed_telegram_usernames = (lambda s: s.split(',') if s else [])(env_allowed_users)
+allowed_telegram_usernames = (lambda s: s.split(',') if s else [])(os.environ.get('ALLOWED_TELEGRAM_USERNAMES', ''))
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = os.environ["MONGODB_URI"] if os.environ.get("MONGODB_URI") else f"mongodb://mongo:{os.environ['MONGODB_PORT']}"
+mongodb_uri = os.environ["MONGO_URL"] if os.environ.get("MONGO_URL") else f"mongodb://mongo:{os.environ['MONGODB_PORT']}"
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:

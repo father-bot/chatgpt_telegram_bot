554dd38890dc7ed2a49c7616ec7519a9ea620bc1
Alexandre loubeyres <alexandre.loubeyres@gmail.com>
2023-04-27 17:10:22 +0200

    last int

--- bot/config.py
+++ bot/config.py
@@ -18,7 +18,7 @@ if config_env["allowed_telegram_usernames"]:
     allowed_telegram_usernames = json.loads(config_env["allowed_telegram_usernames"])
 else:
     allowed_telegram_usernames = []
-new_dialog_timeout = config_env["new_dialog_timeout"]
+new_dialog_timeout = int(config_env["new_dialog_timeout"])
 enable_message_streaming = bool(config_env.get("enable_message_streaming", True))
 return_n_generated_images = int(config_env.get("return_n_generated_images", 1))
 n_chat_modes_per_page = int(config_env.get("n_chat_modes_per_page", 5))

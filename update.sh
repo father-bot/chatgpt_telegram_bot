f7d352202946eb0b64b972a917a43205ddd1763b
Santiago <s@yop.li>
2023-05-01 18:51:20 +0400

    Better update script

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 docker rm -f $(docker ps -q)
 
+git reset --hard HEAD
+
 git pull
+
+chmod +x start.sh
+chmod +x stop.sh
+chmod +x update.sh
 
 docker-compose --env-file config/config.env up --build --detach

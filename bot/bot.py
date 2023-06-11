ec4d74c46bf1b72f76f87a0fea32737221a5a174
Rustam SecondFry Gubaydullin <secondfry+github@gmail.com>
2023-06-11 20:31:40 +0000

    Set logging level to INFO for the bot

--- bot/bot.py
+++ bot/bot.py
@@ -37,7 +37,7 @@ import openai_utils
 
 # setup
 db = database.Database()
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 user_semaphores = {}

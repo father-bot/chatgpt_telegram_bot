0edef0df8856f0a546d2ad599621510a9eba2cd5
Evgeney Kuznetsov <ekuznetsov@devexperts.com>
2023-07-01 00:55:37 +0100

    Removed mongo express Fix for max tokens for completion for GPT 3.5

--- bot/openai_utils.py
+++ bot/openai_utils.py
@@ -7,7 +7,7 @@ openai.api_key = config.openai_api_key
 
 OPENAI_COMPLETION_OPTIONS = {
     "temperature": 0.7,
-    "max_tokens": 4097,
+    "max_tokens": 1000,
     "top_p": 1,
     "frequency_penalty": 0,
     "presence_penalty": 0
